c21dae1f7dd6d4027d3fa394df5cbd6f6d28eec8
Maria Mao <yiping@med.umich.edu>
2023-03-28 15:12:50 -0400

    initial commit

---
+++ code/gdp_lifeexp_analysis.R
@@ -0,0 +1,99 @@
+library(tidyverse)
+
+
+gapminder_1997 <-read.csv("gapminder_1997.csv")
+str(gapminder_1997)
+
+?read_csv
+
+sum(5,6)
+round(3.1415, 3)
+round(2, 3.1415)
+round(x = 3.1415, digits =2)
+
+read_excel()
+
+ggplot(data = gapminder_1997) +
+  aes(x = gdpPercap) +
+  labs(x="GDP per Capita") +
+  aes(y= lifeExp) +
+  labs(y= "Life Expectancy") + 
+  geom_point() +
+   labs(title = " Do people in wealthy countries live longer?") +
+     aes(color = continent) +
+   scale_color_brewer(palette = "Set1") +
+   aes(size=pop/1000000)   +
+  labs(size="population(in millions)")
+
+ggplot(data=gapminder1997)+
+ aes(x=gdpPercap,y=lifeExp,color=continent,size=pop/1000000)+
+  geom_point()+
+  scale_color_brewer(palette="set1") +
+  labs(x="GDP per Capita", y="life Expectancya",title="Do people live longer")
+
+# load in a Larger data set
+
+gapminder_data <-read_csv("gapminder_data.csv")
+dim(gapminder_data)
+ggplot(data=gapminder_data)+
+  aes(x=year,y=lifeExp,group=country,color=continent)+
+  geom_line()
+
+#Discrete plots
+
+ggplot(data=gapminder_1997, aes(x=continent,y=lifeExp))+
+   geom_violin()+
+  geom_jitter(aes(size=pop),color="pink")
+
+ggplot(data=gapminder_1997, aes(x=continent,y=lifeExp))+
+  geom_violin(aes(fill=continent
+        
+ ggplot(data=gapminder_1997, aes(x=continent,y=lifeExp))+
+   geom_violin(fill='springgreen')
+ 
+ 
+ #Univariate plots
+ 
+ ggplot(gapminder_1997)+
+   aes(x=lifeExp)+
+  geom_histogram(bins=5)
+ 
+ #Plot themes
+ 
+ ggplot(gapminder_1997)+
+   aes(x=lifeExp)+
+   geom_histogram(bin=20)+
+   theme_classic()+
+   theme(axis.text.x=element_text(angel=90,vjust=0.5,hjust=1)) 
+ 
+ #facet wrap
+ ggplot(gapminder_1997)+
+   aes(x=gdpPercap,y=lifeExp)+
+   geom_point()+
+   facet_wrap(vars(continent))
+ 
+ #facet grid
+ ggplot(gapminder_1997)+
+   aes(x=gdpPercap,y=lifeExp)+
+   geom_point()+
+   facet_grid(row=vars(continent))
+ 
+ 
+ ggsave(awesome_plot,file="awesome.jpg",width=6,height=4)
+
+#Excercise:
+#-violin plot of continent and life expectancy,
+#-color mapped to continent
+#assigned to an object :violin plot"
+  #-Black and white theme
+  #-save it as "awesome_violon_plot.jpg"
+  
+  violin_plot<-ggplot(gapminder_1997)+
+    aes(x=continent,y=lifeExp)+
+    aes(color = continent) +
+    geom_violin(color="black")+
+    theme_bw()
+  
+  
+    ggsave(violin_plot,file="awesome_plot.jpg",width=6,height=4)
+    
